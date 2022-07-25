a6944408df4e14a81942db5fd4770e6ead88422f
tahacagrimen <tahacagrimen2@gmail.com>
2022-07-25 16:38:41 +0300

    1

--- app.js
+++ app.js
@@ -54,7 +54,7 @@ app.get("/about", pageController.getAboutPage);
 app.get("/add", pageController.getAddPage);
 app.get("/photos/edit/:id", pageController.getEditPage);
 
-const port = 3000;
+const port = process.env.PORT || 5000;
 app.listen(port, () => {
   console.log(`Sunucu ${port} portunda başlatıldı..`);
 });
